5e6d5a838ae8850b568e720c66cad205fb3611f8
Rayra Bulcão <rayra@localhost.localdomain>
2017-11-14 08:53:40 -0400

    Realizando modificações no controller e view de detalhar e editar

--- SalesControl Soft/scripts/app/detalhar/controller-produto-detalhar.js
+++ SalesControl Soft/scripts/app/detalhar/controller-produto-detalhar.js
@@ -8,10 +8,22 @@ angular.module('salescontrolapp')
         $scope.id = 0;
 
         $scope.init = function () {
-            $scope.produto = {
+            $scope.id = $state.params.id;
+            $scope.buscarProduto($scope.id);
                     
-    };
+        }
+
+        // Busca o produto na API
+    $scope.buscarProduto = function(id){
+        $http.get("http://127.0.0.1:8080/#!/" + id).then(function(retorno){
+            $scope.produto = retorno.data[0];
+        }).catch(function(erro){
+            toastr.error("Ocorreu um erro! Tente novamente.");
+        });
+    }
+
+    init();
     }
 
 
-    }
+    
