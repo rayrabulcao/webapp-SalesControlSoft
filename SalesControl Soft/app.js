fedb5bcb6d887ba67e253e93998657aeb2d8f73b
Rayra Bulcão <rayra.bulcao@hotmail.com>
2017-12-02 16:42:26 -0300

    Incluindo rotas de acesso

--- SalesControl Soft/app.js
+++ SalesControl Soft/app.js
@@ -19,31 +19,56 @@ function routerConfig ($stateProvider, $urlRouterProvider){
         url: "/home",
         templateUrl: "scripts/app/home/home.html",
         controller: "HomeController",
-        controllerAs: "controller"
+        controllerAs: "controller",
+        resolve: {
+		    checkRoles: function(RouteAccessService, Profile) {
+		        return RouteAccessService.checkRoles(Profile.isAdmin());
+		    }
+		}
     });
     $stateProvider.state("listar", {
         url: "/listar",
         templateUrl: "scripts/app/listar/listar.html",
         controller: "ListarController",
-        controllerAs: "controller"
+        controllerAs: "controller",
+        resolve: {
+		    checkRoles: function(RouteAccessService, Profile) {
+		        return RouteAccessService.checkRoles(Profile.isAdmin() || Profile.isCliente());
+		    }
+		}
     });
     $stateProvider.state("cadastrar", {
         url: "/cadastrar",
         templateUrl: "scripts/app/cadastrar/cadastrar.html",
         controller: "CadastroController",
-        controllerAs: "controller"
+        controllerAs: "controller",
+        resolve: {
+		    checkRoles: function(RouteAccessService, Profile) {
+		        return RouteAccessService.checkRoles(Profile.isAdmin());
+		    }
+		}
     });
     $stateProvider.state("editar", {
         url: "/editar/:id",
         templateUrl: "scripts/app/editar/editar.html",
         controller: "EditarController",
-        controllerAs: "controller"
+        controllerAs: "controller",
+        resolve: {
+		    checkRoles: function(RouteAccessService, Profile) {
+		        return RouteAccessService.checkRoles(Profile.isAdmin());
+		    }
+		}
     });
     $stateProvider.state("detalhar", {
         url: "/detalhar/:id",
         templateUrl: "scripts/app/detalhar/detalhar.html",
         controller: "DetalharController",
-        controllerAs: "controller"
+        controllerAs: "controller",
+        resolve: {
+		    checkRoles: function(RouteAccessService, Profile) {
+		        return RouteAccessService.checkRoles(Profile.isAdmin() || Profile.isCliente());
+		    }
+		}
     });
     $stateProvider.state("login", {
         url: "/login",
